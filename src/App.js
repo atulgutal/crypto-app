a87a8ec76cdb5e68e447cd82cfb86dbd93bf0b84
atul <atulgutal007@gmail.com>
2021-09-18 18:28:39 -0700

    Navbar implemented

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React from "react";
 import { Switch, Route, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
+import { Navbar } from "./components";
+import "./App.css";
+
 const App = () => {
   return (
     <div className="app">
-      <div className="navbar"></div>
-      <div className="main"></div>
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Switch>
+              <Route exact path="/">
+                <Homepage />
+              </Route>
+            </Switch>
+          </div>
+        </Layout>
+      </div>
       <div className="footer"></div>
     </div>
   );
